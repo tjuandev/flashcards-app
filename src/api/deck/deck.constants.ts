7da5031901249c3a7c1c5a65347af8a780b4acbe
tjuandev <tpydeveloper@gmail.com>
2025-05-09 22:43:27 -0300

    chore: add post and get decks logic

---
+++ src/api/deck/deck.constants.ts
@@ -0,0 +1,7 @@
+export const ERROR_MESSAGES = {
+	DECK_CREATION_FAILED: 'Deck creation failed'
+}
+
+export const SUCCESS_MESSAGES = {
+	DECK_CREATION_SUCCESS: 'Deck created successfully'
+}
--- src/api/deck/deck.controller.ts
+++ src/api/deck/deck.controller.ts
@@ -1,21 +1,31 @@
 import type { NextFunction, Request, Response } from 'express'
-import { type Deck, decks } from './deck.model.ts'
+import { pool } from '#src/config/db.ts'
+import { ERROR_MESSAGES, SUCCESS_MESSAGES } from './deck.constants.ts'
 import { CreateDeckSchema } from './deck.validator.ts'
 
-export const createDeck = (req: Request, res: Response, next: NextFunction) => {
+export const createDeck = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
 	try {
 		const { name } = CreateDeckSchema.parse(req.body)
-		const newDeck: Deck = { id: Date.now(), name }
-		decks.push(newDeck)
-		res.status(201).json(newDeck)
+		await pool.query('INSERT INTO decks(name) VALUES($1)', [name])
+		res.status(201).json({ message: SUCCESS_MESSAGES.DECK_CREATION_SUCCESS })
 	} catch (error) {
+		res.status(400).json({ message: ERROR_MESSAGES.DECK_CREATION_FAILED })
 		next(error)
 	}
 }
 
-export const getDecks = (_req: Request, res: Response, next: NextFunction) => {
+export const getDecks = async (
+	_req: Request,
+	res: Response,
+	next: NextFunction
+) => {
 	try {
-		res.json(decks)
+		const { rows } = await pool.query('SELECT * FROM decks')
+		res.json(rows).status(200)
 	} catch (error) {
 		next(error)
 	}
