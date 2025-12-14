d826a25c04acf575a686d62565304330eb2247fe
tjuandev <tpydeveloper@gmail.com>
2025-12-14 17:50:32 -0300

    chore: update mocked user_id

--- src/api/deck/repository.ts
+++ src/api/deck/repository.ts
@@ -1,7 +1,7 @@
 import { pool } from '#src/config/db.ts'
 
 // TODO: Remove this once we have an authentication feature
-const MOCKED_USER_ID = '0e7725d1-24d5-4241-9ff1-38b03d02e20e'
+const MOCKED_USER_ID = '36ca3552-48b6-4f48-a7dc-8ae11c352412'
 
 export async function createDeck(name: string) {
 	return await pool.query('INSERT INTO decks(name, user_id) VALUES($1, $2)', [
